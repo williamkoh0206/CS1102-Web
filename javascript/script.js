d5b710d8cb89cb56910d058c6d614e227ad86527
williamkoh0206 <111341756+williamkoh0206@users.noreply.github.com>
2023-12-01 05:05:18 +0800

    update the mixed reality page & css

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,10 @@ const pressMeButton = document.querySelector(".press-me");
 const introContent = document.querySelector(".introduction-content");
 const afterContent = document.querySelectorAll(".after");
 const techniqueContent = document.querySelectorAll(".technique-after");
+const mrBtn = document.querySelectorAll(".mr-tabs");
+const mrContent = document.querySelectorAll(".mr-content");
+const mrInnerContent = document.querySelectorAll(".mr-inner-content");
+const defaultOpenElement = document.getElementById("defaultOpen");
 
 //eventListener
 pressMeButton.addEventListener("click", () => {
@@ -20,4 +24,26 @@ pressMeButton.addEventListener("click", () => {
       el.style.opacity = el.style.opacity === "0" ? "1" : "0";
     });
   }
+  if (window.location.href.includes("MixedReality.html")) {
+    mrInnerContent.forEach((el) => {
+      el.style.opacity = el.style.opacity === "0" ? "1" : "0";
+    });
+  }
 });
+
+//Mixed Reality Page
+if(window.location.href.includes("MixedReality.html")){
+  function openMrtabs(event,content){
+    for (i = 0; i < mrContent.length; i++) {
+      mrContent[i].style.display = "none";
+    }
+    for (i = 0; i < mrBtn.length; i++) {
+      mrBtn[i].className = mrBtn[i].className.replace(" active", "");
+    }
+    document.getElementById(content).style.display = "block";
+    event.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  defaultOpenElement.click();
+}
